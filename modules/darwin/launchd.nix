29706f4fdb989259a7db8fdec6f87baa570d3a07
Ian Pratt <ianjdpratt@gmail.com>
2025-02-20 09:26:51 +0000

    79   2025-02-20 09:19:26   (current)

--- modules/darwin/launchd.nix
+++ modules/darwin/launchd.nix
@@ -1,10 +1,13 @@
 # NOTE: See:
 #   - https://www.danielcorin.com/til/nix-darwin/launch-agents/
+#   - https://daiderd.com/nix-darwin/manual/index.html#opt-launchd.user.agents._name_.serviceConfig
+#   - https://daiderd.com/nix-darwin/manual/index.html#opt-launchd.user.agents._name_.serviceConfig.StartCalendarInterval
 {pkgs, ...}: {
   launchd.user.agents = {
     # Open raycast on startup
     raycast = {
       command = "open ${pkgs.raycast}/Applications/Raycast.app";
+
       serviceConfig = {
         KeepAlive = false; # When it stops, do *NOT* restart it (otherwise it just keeps opening lmao :p)
         RunAtLoad = true;
