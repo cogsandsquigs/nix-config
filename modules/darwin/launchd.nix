faf49dc3bc5501da24db901064c1d66db262a84e
Ian Pratt <ianjdpratt@gmail.com>
2025-02-20 09:04:19 +0000

    74   2025-02-20 09:04:06   (current)

--- modules/darwin/launchd.nix
+++ modules/darwin/launchd.nix
@@ -2,6 +2,12 @@
   launchd.user.agents = {
     raycast = {
       command = "${pkgs.raycast}";
+      serviceConfig = {
+        KeepAlive = true;
+        RunAtLoad = true;
+        StandardOutPath = "/tmp/raycast.out.log";
+        StandardErrorPath = "/tmp/raycast.err.log";
+      };
     };
   };
 }
