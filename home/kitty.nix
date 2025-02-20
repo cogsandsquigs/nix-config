686a3bde9d3abe49c2a4ddcc2e010ca7b9dd5d30
Ian Pratt <ianjdpratt@gmail.com>
2025-02-20 06:55:28 +0000

    59   2025-02-20 06:55:19   (current)

--- home/kitty.nix
+++ home/kitty.nix
@@ -1,3 +1,34 @@
-{
-  programs.kitty = {};
+{pkgs, ...}: {
+  programs.kitty = {
+    enable = true;
+
+    darwinLaunchOptions = ["--start-as fullscreen"];
+
+    font = {
+      name = "FiraCode Nerd Font Mono";
+      package = pkgs.nerd-fonts.fira-code;
+      size = 13;
+    };
+
+    keybindings = {
+      # Linux keybinds
+      "super+d" = "new_window";
+      "super+]" = "next_window";
+      "super+[" = "previous_window";
+
+      # MacOS keybinds
+      "cmd+d" = "new_window";
+      "cmd+]" = "next_window";
+      "cmd+[" = "previous_window";
+    };
+
+    themeFile = "Catppuccin-Mocha";
+
+    settings = {
+      "cursor_shape" = "beam";
+      "modify_font cell_width" = "+0px";
+      "modify_font cell_height" = "+0px";
+      "enabled_layouts" = "tall:bias=50;full_size=1;mirrored=false";
+    };
+  };
 }
