26900aa6d4040fa78213dd19bf10960bd2ac3459
Ian Pratt <ianjdpratt@gmail.com>
2025-02-21 01:27:56 +0000

    105   2025-02-21 01:27:47   (current)

--- home/zsh.nix
+++ home/zsh.nix
@@ -12,7 +12,7 @@
       "source /etc/profile.d/nix.sh\n" # Source nix environment variables
       + "source ~/.p10k.zsh\n"; # Source powerlevel10k theme
 
-    envExtra = "export $EDITOR=nvim"; # Set default editor to nvim
+    envExtra = "export EDITOR=nvim"; # Set default editor to nvim
 
     plugins = [
       {
