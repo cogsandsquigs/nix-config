e21b81789c6241ce9ae63fd62aecb548dc63806b
Ian Pratt <ianjdpratt@gmail.com>
2025-03-01 23:50:14 +0000

    Nix rebuild

--- home/librewolf.nix
+++ home/librewolf.nix
@@ -1,2 +1,16 @@
 {
+  programs.librewolf = {
+    enable = true;
+    settings = {
+      "webgl.disabled" = false;
+
+      "identity.fxaccounts.enabled" = false; # Enable/Disable firefox sync
+
+      "privacy.clearOnShutdown.history" = false;
+      "privacy.clearOnShutdown.downloads" = true;
+      "privacy.resistFingerprinting" = true;
+      "privacy.userContext.enabled" = true; # Enable containers
+      "privacy.userContext.ui.enabled" = true; # Enable containers UI
+    };
+  };
 }
