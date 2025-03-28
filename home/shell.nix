6ff3a0dd1ac9308d742c01d785653d1bd95ce7ee
Ian Pratt <ianjdpratt@gmail.com>
2025-03-28 18:13:42 +0000

    Nix rebuild

--- home/shell.nix
+++ home/shell.nix
@@ -15,7 +15,9 @@
 
     envExtra =
       "export EDITOR=nvim\n" # Set default editor to nvim
-      + "export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which java))))\n";
+      + "export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which java))))\n"
+      + "export CC=${pkgs.clang}"
+      + "export AR=${pkgs.llvm}";
 
     plugins = [
       {
