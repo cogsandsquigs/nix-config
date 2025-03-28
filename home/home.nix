f6734a112b598b7fa19ebfd79e77c0caa0f0e3d5
Ian Pratt <ianjdpratt@gmail.com>
2025-03-28 22:47:21 +0000

    Nix rebuild

--- home/home.nix
+++ home/home.nix
@@ -14,98 +14,100 @@
   ];
 
   # User-only packages
-  home.packages = with pkgs; [
-    ## SHELLS ##
+  home.packages = with pkgs;
+    [
+      ## SHELLS ##
 
-    # Zsh
-    zsh
-    oh-my-zsh
-    zsh-powerlevel10k
+      # Zsh
+      zsh
+      oh-my-zsh
+      zsh-powerlevel10k
 
-    # Fish
-    fish
+      # Fish
+      fish
 
-    ## USERLAND ##
+      ## USERLAND ##
 
-    kitty # Terminal
-    # neovim # Editor NOTE: Removed, see here: https://discourse.nixos.org/t/home-manager-neovim-collision/16963
-    git # <3
-    lazygit # Makes git awesomer
-    gnupg # Signatures
-    delta # Git diff highlighting
-    zoxide # Better CD
-    fzf # Fuzzy finder
-    eza # Better ls
-    dust # Better du
-    bat # Better cat
-    ripgrep
-    neofetch
-    jq
+      kitty # Terminal
+      # neovim # Editor NOTE: Removed, see here: https://discourse.nixos.org/t/home-manager-neovim-collision/16963
+      git # <3
+      lazygit # Makes git awesomer
+      gnupg # Signatures
+      delta # Git diff highlighting
+      zoxide # Better CD
+      fzf # Fuzzy finder
+      eza # Better ls
+      dust # Better du
+      bat # Better cat
+      ripgrep
+      neofetch
+      jq
 
-    ## DEVELOPMENT ##
+      ## DEVELOPMENT ##
 
-    # C/C++
-    clang
-    cmake
-    libiconv
-    llvmPackages.clang-tools # tools for clang!
+      # C/C++
+      clang
+      cmake
+      llvmPackages.clang-tools # tools for clang!
 
-    # Docker/VMs
-    docker
-    docker-compose
-    colima
-    lima
+      # Docker/VMs
+      docker
+      docker-compose
+      colima
+      lima
 
-    # Documentation generation
-    mdbook # Docs from MD: https://rust-lang.github.io/mdBook/index.html
+      # Documentation generation
+      mdbook # Docs from MD: https://rust-lang.github.io/mdBook/index.html
 
-    # Java/Kotlin
-    jdk
-    gradle
-    kotlin
-    jetbrains.idea-ultimate
+      # Java/Kotlin
+      jdk
+      gradle
+      kotlin
+      jetbrains.idea-ultimate
 
-    # Latex
-    texliveFull # Install `latexmk` + co (unneeded) for vimtex (see neovim config)
+      # Latex
+      texliveFull # Install `latexmk` + co (unneeded) for vimtex (see neovim config)
 
-    # Nix
-    alejandra # Formatter
-    # direnv # NOTE: Not needed! See: https://github.com/nix-community/nix-direnv
+      # Nix
+      alejandra # Formatter
+      # direnv # NOTE: Not needed! See: https://github.com/nix-community/nix-direnv
 
-    # Nodejs
-    nodejs
-    bun
+      # Nodejs
+      nodejs
+      bun
 
-    # Python
-    python3
-    black # Formatter
-    pyright # Typechecker
-    pylint # Linter
+      # Python
+      python3
+      black # Formatter
+      pyright # Typechecker
+      pylint # Linter
 
-    # Ruby
-    rbenv
+      # Ruby
+      rbenv
 
-    # Rust
-    rustup
-    #cargo-afl # Fuzzing
-    cargo-watch
-    cargo-workspaces
+      # Rust
+      rustup
+      #cargo-afl # Fuzzing
+      cargo-watch
+      cargo-workspaces
 
-    ## FUN ##
-    # modrinth-app # Minecraft launcher # NOTE: Disabled for now due to issues w/ compile
-    ghidra-bin # just a lil silly :3
+      ## FUN ##
+      # modrinth-app # Minecraft launcher # NOTE: Disabled for now due to issues w/ compile
+      ghidra-bin # just a lil silly :3
 
-    ## MISCELLANEOUS ##
-    magic-wormhole
-    discord # For some reason discord is availabe on mac via nixpkgs, but not firefox???
-    fontconfig
-    obsidian
-    # NOTE: Why `firefox-unwrapped` and not `firefox`?
-    # See: https://github.com/NixOS/nixpkgs/issues/366581
-    # NOTE: Why no use? B/c not preserving firefox config thru reinstalls.
-    # So use homebrew to install on mac
-    # firefox-unwrapped
-    librewolf
-    spotify
-  ];
+      ## MISCELLANEOUS ##
+      magic-wormhole
+      discord # For some reason discord is availabe on mac via nixpkgs, but not firefox???
+      fontconfig
+      obsidian
+      # NOTE: Why `firefox-unwrapped` and not `firefox`?
+      # See: https://github.com/NixOS/nixpkgs/issues/366581
+      # NOTE: Why no use? B/c not preserving firefox config thru reinstalls.
+      # So use homebrew to install on mac
+      # firefox-unwrapped
+      librewolf
+      spotify
+    ]
+    # Additional libraries needed for (C) development
+    ++ (lib.optional stdenv.isDarwin pkgs.darwin.libiconv);
 }
