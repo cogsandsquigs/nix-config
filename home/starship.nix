b4b6b2063888deb05c7498ba134b855d0e1ccf1e
Ian Pratt <ianjdpratt@gmail.com>
2025-05-23 12:05:12 +0100

    Nix rebuild

--- home/starship.nix
+++ home/starship.nix
@@ -4,8 +4,10 @@
         enableFishIntegration = true;
 
         # Equivalent to writting starship.toml
+        # TODO: Use this at some point: https://starship.rs/presets/catppuccin-powerline#catppuccin-powerline-preset
         settings = {
-            format = "$all";
+            format = "$all"; # Default, whatev
+            add_newline = false; # Disable the blank line at the start of the prompt
         };
     };
 }
