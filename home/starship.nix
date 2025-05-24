00e8d47d10bfd21b3f0050d028a8a2af6e6641be
Ian Pratt <ianjdpratt@gmail.com>
2025-05-24 08:04:29 +0100

    Nix rebuild

--- home/starship.nix
+++ home/starship.nix
@@ -6,8 +6,11 @@
         # Equivalent to writting starship.toml
         # TODO: Use this at some point: https://starship.rs/presets/catppuccin-powerline#catppuccin-powerline-preset
         settings = {
-            format = "$all"; # Default, whatev
             add_newline = false; # Disable the blank line at the start of the prompt
+            format = ''
+                [┌───────────────────>](bold green)
+                [│](bold green)$directory$rust$package
+                [└─>](bold green)'';
         };
     };
 }
