3498f848f9472e4cbf2825d62616b981d1f52629
Ian Pratt <ianjdpratt@gmail.com>
2025-05-24 09:05:56 +0100

    Nix rebuild

--- home/starship.nix
+++ home/starship.nix
@@ -29,7 +29,7 @@
             # Directory/cwd
             directory = {
                 format = "[$path]($style)[$read_only]($read_only_style) ";
-                style = "bold bright-blue";
+                style = "bold bright-cyan";
 
                 ###########
                 # SYMBOLS #
