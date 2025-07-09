e3644849113b747e8df6048c8ad8f3970847916c
Ian Pratt <ianjdpratt@gmail.com>
2025-07-09 23:41:08 +0100

    Nix rebuild

--- home/all/configs/zellij.nix
+++ home/all/configs/zellij.nix
@@ -2,7 +2,7 @@
     programs.zellij = {
         enable = true;
         enableZshIntegration = true;
-        enableFishIntegration = false; # ?
+        enableFishIntegration = false; # For some reason zellij is slow rn?
         enableBashIntegration = true;
 
         package = pkgs.zellij;
