c9fba7ce6a0b5d3d95de9461789ba0df91c2c678
Ian Pratt <ianjdpratt@gmail.com>
2025-07-09 23:36:20 +0100

    Nix rebuild

--- home/all/configs/zellij.nix
+++ home/all/configs/zellij.nix
@@ -2,13 +2,13 @@
     programs.zellij = {
         enable = true;
         enableZshIntegration = true;
-        enableFishIntegration = true;
+        enableFishIntegration = false; # ?
         enableBashIntegration = true;
 
         package = pkgs.zellij;
 
         settings = {
-            theme = "catppuccin-macciato";
+            theme = "catppuccin-mocha";
         };
     };
 }
