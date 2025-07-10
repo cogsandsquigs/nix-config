c21ef7db5c7239a637d3990a84eb254f69ec43ca
Ian Pratt <ianjdpratt@gmail.com>
2025-07-10 22:44:58 +0100

    Nix rebuild

--- home/all/configs/helix.nix
+++ home/all/configs/helix.nix
@@ -1,143 +1,141 @@
 {pkgs, ...}: {
-    programs.helix = {
-        enable = true;
-        package = pkgs.helix;
+  programs.helix = {
+    enable = true;
+    package = pkgs.helix;
 
-        # General settings
-        # See: https://docs.helix-editor.com/configuration.html
-        settings = {
-            theme = "catppuccin_mocha";
+    # General settings
+    # See: https://docs.helix-editor.com/configuration.html
+    settings = {
+      theme = "catppuccin_mocha";
 
-            # Keys in normal-mode (not highlighting/selecting or inserting
-            # text).
-            keys.normal = {
-                # Keys in space-mode (after pressing leader/space)
-                # See: https://github.com/helix-editor/helix/issues/2841
-                space = let
-                    floating_pane_size_percent = 80;
-                    side_pane_size_percent = 30;
+      # Keys in normal-mode (not highlighting/selecting or inserting
+      # text).
+      keys.normal = {
+        # Keys in space-mode (after pressing leader/space)
+        # See: https://github.com/helix-editor/helix/issues/2841
+        space = let
+          floating_pane_size_percent = 80;
 
-                    # Opens a Zellij floating pane of height and width
-                    # `floating_pane_size_percent` percent of the screen.
-                    # NOTE: Requires Zellij to be installed and configured.
-                    make_zellij_floating_pane = cmd: [
-                        ":sh zellij run --close-on-exit --height ${builtins.toString floating_pane_size_percent}% --width ${builtins.toString floating_pane_size_percent}% --floating -x ${builtins.toString ((100 - floating_pane_size_percent) / 2)}% -y ${builtins.toString ((100 - floating_pane_size_percent) / 2)}% -- ${cmd}"
-                    ];
-                in {
-                    # Opens a lazygit floating pane via `space-l-g`.
-                    l.g = make_zellij_floating_pane "lazygit";
+          # Opens a Zellij floating pane of height and width
+          # `floating_pane_size_percent` percent of the screen.
+          # NOTE: Requires Zellij to be installed and configured.
+          make_zellij_floating_pane = cmd: [
+            ":sh zellij run --close-on-exit --height ${builtins.toString floating_pane_size_percent}% --width ${builtins.toString floating_pane_size_percent}% --floating -x ${builtins.toString ((100 - floating_pane_size_percent) / 2)}% -y ${builtins.toString ((100 - floating_pane_size_percent) / 2)}% -- ${cmd}"
+          ];
+        in {
+          # Opens a lazygit floating pane via `space-l-g`.
+          l.g = make_zellij_floating_pane "lazygit";
 
-                    # Opens a terminal-interface floating pane via `space-t`.
-                    t = make_zellij_floating_pane "$SHELL";
+          # Opens a terminal-interface floating pane via `space-t`.
+          t = make_zellij_floating_pane "$SHELL";
 
-                    # Opens a file picker using `nnn` via via `space-f`.
-                    # NOTE: Overrides the default helix file picker!
-                    # f = make_zellij_floating_pane "nnn -d";
-                
-                    # [keys.normal.C-y]
-# Open the file(s) in the current window
-y = ":sh zellij run -n Yazi -c -f -x 10% -y 10% --width 80% --height 80% -- bash ~/.config/helix/yazi-picker.sh open";
-# Open the file(s) in a vertical split
-v = ":sh zellij run -n Yazi -c -f -x 10% -y 10% --width 80% --height 80% -- bash ~/.config/helix/yazi-picker.sh vsplit";
-# Open the file(s) in a horizontal split
-h = ":sh zellij run -n Yazi -c -f -x 10% -y 10% --width 80% --height 80% -- bash ~/.config/helix/yazi-picker.sh hsplit";
-                };
-            };
+          # Opens a file picker using `nnn` via via `space-f`.
+          # NOTE: Overrides the default helix file picker!
+          # f = make_zellij_floating_pane "nnn -d";
 
-            editor = {
-                mouse = true; # Allow use of the mouse
-                gutters = ["diagnostics" "spacer" "line-numbers" "spacer" "diff"];
+          # [keys.normal.C-y]
+          # Open the file(s) in the current window
+          y = ":sh zellij run -n Yazi -c -f -x 10% -y 10% --width 80% --height 80% -- bash ~/.config/helix/yazi-picker.sh open";
+          # Open the file(s) in a vertical split
+          v = ":sh zellij run -n Yazi -c -f -x 10% -y 10% --width 80% --height 80% -- bash ~/.config/helix/yazi-picker.sh vsplit";
+          # Open the file(s) in a horizontal split
+          h = ":sh zellij run -n Yazi -c -f -x 10% -y 10% --width 80% --height 80% -- bash ~/.config/helix/yazi-picker.sh hsplit";
+        };
+      };
 
-                statusline = {
-                    left = ["mode" "version-control"];
-                    center = ["file-name" "file-modification-indicator" "diagnostics"];
-                    right = ["file-type" "file-encoding" "spinner" "register"];
+      editor = {
+        mouse = true; # Allow use of the mouse
+        gutters = ["diagnostics" "spacer" "line-numbers" "spacer" "diff"];
+        auto-format = true;
+        statusline = {
+          left = ["mode" "version-control"];
+          center = ["file-name" "file-modification-indicator" "diagnostics"];
+          right = ["file-type" "file-encoding" "spinner" "register"];
 
-                    separator = "|";
+          separator = "|";
 
-                    mode.normal = "NORMAL";
-                    mode.insert = "INSERT";
-                    mode.select = "SELECT";
-                };
-
-                cursor-shape = {
-                    insert = "bar";
-                    normal = "block";
-                    select = "underline";
-                };
-
-                whitespace = {
-                    render = {
-                        tab = "all";
-                        space = "all";
-                        nbsp = "none";
-                        nnbsp = "none";
-                        newline = "none";
-                    };
-
-                    characters = {
-                        tab = "→";
-                        tabpad = " "; # Tabs will look like this: "→   "
-                        space = " "; # NOTE: only doing this b/c then spaces btwn words get annoying characters in them
-                    };
-                };
-
-                indent-guides = {
-                    render = true;
-                    character = "│";
-                    skip-levels = 1;
-                };
-
-                inline-diagnostics = {
-                    cursor-line = "hint";
-                    other-lines = "hint";
-                };
-            };
+          mode.normal = "NORMAL";
+          mode.insert = "INSERT";
+          mode.select = "SELECT";
         };
 
-        # Language-specific settings
-        # See: https://docs.helix-editor.com/languages.html
-        languages = {
-            # Language-server settings.
-            language-server = {
-                rust-analyzer = {
-                    command = "rust-analyzer";
-
-                    # See: https://rust-analyzer.github.io/book/configuration
-                    config.rust-analyzer = {
-                        check.command = "clippy";
-                        procMacro.enable = true;
-                        cargo.buildScripts.enable = true;
-                    };
-                };
-
-                nil = {
-                    command = "nil";
-                    args = ["--stdio"];
-                };
-            };
-
-            # Language configurations for each language.
-            language = [
-                {
-                    name = "rust";
-                    language-servers = ["rust-analyzer"];
-                    formatter = {
-                        command = "rustfmt";
-                        args = [];
-                    };
-                }
-
-{
-                    name = "nix";
-                    language-servers = ["nil"];
-                    formatter = {
-                        command = "alejandra";
-                        args = [];
-                    };
-                }
-                
-            ];
+        cursor-shape = {
+          insert = "bar";
+          normal = "block";
+          select = "underline";
         };
+
+        whitespace = {
+          render = {
+            tab = "all";
+            space = "all";
+            nbsp = "none";
+            nnbsp = "none";
+            newline = "none";
+          };
+
+          characters = {
+            tab = "→";
+            tabpad = " "; # Tabs will look like this: "→   "
+            space = " "; # NOTE: only doing this b/c then spaces btwn words get annoying characters in them
+          };
+        };
+
+        indent-guides = {
+          render = true;
+          character = "│";
+          skip-levels = 1;
+        };
+
+        inline-diagnostics = {
+          cursor-line = "hint";
+          other-lines = "hint";
+        };
+      };
     };
+
+    # Language-specific settings
+    # See: https://docs.helix-editor.com/languages.html
+    languages = {
+      # Language-server settings.
+      language-server = {
+        rust-analyzer = {
+          command = "rust-analyzer";
+
+          # See: https://rust-analyzer.github.io/book/configuration
+          config.rust-analyzer = {
+            check.command = "clippy";
+            procMacro.enable = true;
+            cargo.buildScripts.enable = true;
+          };
+        };
+
+        nil = {
+          command = "nil";
+          args = ["--stdio"];
+        };
+      };
+
+      # Language configurations for each language.
+      language = [
+        {
+          name = "rust";
+          language-servers = ["rust-analyzer"];
+          formatter = {
+            command = "rustfmt";
+            args = [];
+          };
+        }
+
+        {
+          name = "nix";
+          language-servers = ["nil"];
+          formatter = {
+            command = "alejandra";
+            args = [];
+          };
+        }
+      ];
+    };
+  };
 }
--- home/all/packages/languages/nix.nix
+++ home/all/packages/languages/nix.nix
@@ -1,6 +1,8 @@
 {pkgs, ...}: {
+
     home.packages = with pkgs; [
-        alejandra # Formatter
+        alejandra # UN-official formatter (stable, opinionated!)
+        nixfmt-rfc-style # Official (!) formatter (but unstable, so proceed with caution!) NOTE: when stabilized, use `nixfmt`
         nil # Nix LSP
         # direnv # NOTE: Not needed! See: https://github.com/nix-community/nix-direnv
     ];
