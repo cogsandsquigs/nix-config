c49c051969ec63645edc57039066342ee2ef74a6
Ian Pratt <ianjdpratt@gmail.com>
2025-07-16 02:07:37 +0100

    Nix rebuild

--- home/all/configs/helix.nix
+++ home/all/configs/helix.nix
@@ -208,6 +208,38 @@ in
                 }
 
                 {
+                    name = "toml";
+                    auto-format = true;
+                    indent = {
+                        tab-width = 4;
+                        unit = "    ";
+                    };
+                    formatter = {
+                        command = "taplo";
+                        args = [
+                            "fmt"
+                            "-o"
+                            "align_entries=true"
+                            "-o"
+                            "align-comments"
+                            "-o"
+                            "allowed_blank_lines=1"
+                            "-o"
+                            "indent_entries=true"
+                            "-o"
+                            "indent_tables=true"
+                            "-o"
+                            "indent_string=    "
+                            "-o"
+                            "reorder_arrays"
+                            "-o"
+                            "reorder_keys"
+                            "-"
+                        ];
+                    };
+                }
+
+                {
                     name = "nix";
                     language-servers = [ "nil" ];
                     auto-format = true;
