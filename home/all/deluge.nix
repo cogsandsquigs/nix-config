5c4bb61586d1a69f6fbfbf7cb5baa5981086926b
Ian Pratt <ianjdpratt@gmail.com>
2025-08-13 20:42:58 +0100

    Nix rebuild

--- home/all/deluge.nix
+++ home/all/deluge.nix
@@ -1,4 +1,7 @@
 { pkgs, ... }:
 {
-    home.packages = with pkgs; [ deluge ];
+    home.packages = with pkgs; [
+        deluge
+        deluge-gtk
+    ];
 }
--- home/all/transmission.nix
+++
@@ -1,6 +0,0 @@
-{ pkgs, ... }:
-{
-    home.packages = with pkgs; [
-        transmission_4-gtk # Bittorrent client GUI
-    ];
-}
