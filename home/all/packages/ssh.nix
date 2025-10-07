cb5de69f6fa1b96987b23c68d8ec8ddc9ab54042
Ian Pratt <ianjdpratt@gmail.com>
2025-10-07 19:06:36 +0100

    Nix rebuild

--- home/all/packages/ssh.nix
+++ home/all/packages/ssh.nix
@@ -21,6 +21,21 @@ in
 
         # Per-host matching rules
         matchBlocks = {
+            # Default values!
+            # See: https://home-manager-options.extranix.com/?query=programs.ssh&release=master
+            "*" = {
+                forwardAgent = false;
+                addKeysToAgent = "no";
+                compression = false;
+                serverAliveInterval = 0;
+                serverAliveCountMax = 3;
+                hashKnownHosts = false;
+                userKnownHostsFile = "~/.ssh/known_hosts";
+                controlMaster = "no";
+                controlPath = "~/.ssh/master-%r@%n:%p";
+                controlPersist = "no";
+            };
+
             "*" = {
                 identityFile = "~/.ssh/id_ed25519";
                 extraOptions = {
