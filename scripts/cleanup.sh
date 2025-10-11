d2d3c45220f37dab9a6d29a038a67872e0f13e2e
Ian Pratt <ianjdpratt@gmail.com>
2025-10-11 20:47:03 +0100

    Nix rebuild

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+#export NIX_STORE=/nix
+#export DU_CMD=/usr/bin/du
+#export
+
+#dir_size() {
+#    local dir, size
+#
+#    dir=$1
+#    size=$($DU_CMD -B1 -s "$dir" | awk '{sum += $1} END{print sum}')
+#
+#}
+
+#$DU_CMD -B1 -s $NIX_STORE | awk '{sum += $1} END{print sum}'
+
 sudo -i nix-env --delete-generations old
 sudo -i nix-store --gc
 sudo -i nix-collect-garbage -d
--- scripts/editnix.sh
+++ scripts/editnix.sh
@@ -9,4 +9,4 @@ $EDITOR # Open da editorrrr
 
 git add . # Make sure nix sees all changes!
 
-python3 $SCRIPTS_PATH/rebuild.sh
+$SCRIPTS_PATH/rebuild.sh
