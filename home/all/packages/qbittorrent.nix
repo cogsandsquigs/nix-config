63616606075c6aaa569a4b3001de041c49910844
Ian Pratt <ianjdpratt@gmail.com>
2025-10-25 23:44:32 +0100

    Nix rebuild

--- home/all/packages/qbittorrent.nix
+++ home/all/packages/qbittorrent.nix
@@ -1,4 +1,7 @@
 { pkgs, ... }:
 {
-    home.packages = with pkgs; [ qbittorrent ];
+    home.packages = with pkgs; [
+        # For some reason, this breaks on MacOS b/c wayland dependency (??)
+        # qbittorrent
+    ];
 }
