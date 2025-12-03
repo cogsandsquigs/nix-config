e654c505f1d877f74b2fd13a44771567742c024a
Ian Pratt <ianjdpratt@gmail.com>
2025-12-03 02:35:22 +0000

    Nix rebuild

--- home/all/packages/helix.nix
+++ home/all/packages/helix.nix
@@ -173,8 +173,11 @@ in
                     args = [ "--lsp" ];
                     config.haskell = {
                         formattingProvider = "fourmolu";
-                        plugin.fourmolu.config.external = true;
-                        plugin.hlint.config.external = true; # Uses nix-installed hlint, fixes https://github.com/haskell/haskell-language-server/issues/4674
+
+                        plugin = {
+                            fourmolu.config.external = true;
+                            hlint.diagnosticsOn = false; # Fixes https://github.com/haskell/haskell-language-server/issues/4674 until GHC fixed
+                        };
                     };
 
                 };
