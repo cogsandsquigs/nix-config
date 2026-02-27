ba82fa71c17ef61f70c9f88be67bd15c8d888497
Ian Pratt <ianjdpratt@gmail.com>
2026-02-27 03:14:23 +0000

    Nix rebuild

--- home/all/languages/haskell.nix
+++ home/all/languages/haskell.nix
@@ -1,18 +1,20 @@
 { pkgs, ... }:
 {
-    home.packages = with pkgs; [
-        ghc
-        haskellPackages.haskell-language-server
-        haskellPackages.hlint
-
-        # NOTE: As of 2026-02-27, `haskellPackages.cabal-install` and `haskellPackages.fourmolu`
-        # have a conflicting subpath when evaluating them. We will ignore their collisions so both
-        # can be installed together.
-        (haskellPackages.cabal-install.override (args: {
-            ignoreCollisions = true;
-        }))
-        (haskellPackages.fourmolu.override (args: {
-            ignoreCollisions = true;
-        }))
-    ];
+    home.packages =
+        let
+            # NOTE: As of 2026-02-27, `haskellPackages.cabal-install` and `haskellPackages.fourmolu`
+            # have a conflicting subpath when evaluating them. We will ignore their collisions so both
+            # can be installed together.
+            haskellPackages = haskellPackages.override (args: {
+                ignoreCollisions = true;
+            });
+        in
+        with pkgs;
+        [
+            ghc
+            haskellPackages.haskell-language-server
+            haskellPackages.hlint
+            haskellPackages.cabal-install
+            haskellPackages.fourmolu
+        ];
 }
