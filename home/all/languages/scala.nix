f5b4b11c1ed6aa9ad39eb0bf4351e394c6136ba6
Ian Pratt <ianjdpratt@gmail.com>
2026-03-02 19:22:23 +0000

    Nix rebuild

--- home/all/languages/scala.nix
+++ home/all/languages/scala.nix
@@ -2,7 +2,6 @@
 {
     home.packages = with pkgs; [
         scala-next # Latest (stable!) version, `scala` is the LTS version
-        scala-cli # CLI for scala
         bloop # Buildserver
         metals # LSP
         scalafix # Linter
