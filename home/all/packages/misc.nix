122761320a9a809c247f99b345f26faf2f013643
Ian Pratt <ianjdpratt@gmail.com>
2026-03-07 18:07:41 +0000

    Nix rebuild

--- home/all/packages/misc.nix
+++ home/all/packages/misc.nix
@@ -2,7 +2,6 @@
 {
     home.packages = with pkgs; [
         # discord # NOTE: See system/darwin/homebrew.nix for why we use homebrew ver
-        obsidian
         # spotify
         # postman
         zoom-us
