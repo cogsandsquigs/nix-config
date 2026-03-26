c87aa4891573dc6538c0dc1e59ca743e55f628fa
Ian Pratt <ianjdpratt@gmail.com>
2026-03-26 04:16:37 +0000

    Nix rebuild

--- modules/hosts/macbook/home-manager.nix
+++ modules/hosts/macbook/home-manager.nix
@@ -2,6 +2,7 @@
 {
     flake.modules.darwin."Ians-GlorpBook-Pro" = {
         home-manager = {
+            backupCommand = "rm";
             backupFileExtension = "bak";
             users.cogs.imports = with inputs.self.modules.homeManager; [ cogs ];
         };
