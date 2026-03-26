53919ba07b7f5fb28c8420c255d592ccb0a7045b
Ian Pratt <ianjdpratt@gmail.com>
2026-03-26 04:23:25 +0000

    Nix rebuild

--- modules/programs/desktop-apps/browser.nix
+++ modules/programs/desktop-apps/browser.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, ... }:
 {
     flake.modules.homeManager.browser =
         { config, pkgs, ... }:
@@ -9,33 +9,6 @@
                 enable = true;
                 # package = pkgs.librewolf;
                 languagePacks = [ "en-US" ];
-
-                policies = {
-                    # Updates & Background Services
-                    AppAutoUpdate = false;
-                    BackgroundAppUpdate = false;
-
-                    # Feature Disabling
-                    # DisableBuiltinPDFViewer = true;
-                    # DisableFirefoxStudies = true;
-                    # DisableFirefoxAccounts = true;
-                    # DisableFirefoxScreenshots = true;
-                    # DisableForgetButton = true;
-                    # DisableMasterPasswordCreation = true;
-                    # DisableProfileImport = true;
-                    # DisableProfileRefresh = true;
-                    # DisableSetDesktopBackground = true;
-                    DisablePocket = true;
-                    DisableTelemetry = true;
-                    # DisableFormHistory = true;
-                    # DisablePasswordReveal = true;
-
-                    # UI and Behavior
-                    # DisplayMenuBar = "never";
-                    DontCheckDefaultBrowser = true;
-                    HardwareAcceleration = true;
-                    OfferToSaveLogins = false;
-                };
 
                 profiles.default = {
                     settings = {
@@ -74,6 +47,100 @@
                                     }
                                 ];
                             };
+                        };
+                    };
+                };
+
+                policies = {
+                    # Updates & Background Services
+                    AppAutoUpdate = false;
+                    BackgroundAppUpdate = false;
+
+                    # Feature Disabling
+                    # DisableBuiltinPDFViewer = true;
+                    # DisableFirefoxStudies = true;
+                    # DisableFirefoxAccounts = true;
+                    # DisableFirefoxScreenshots = true;
+                    # DisableForgetButton = true;
+                    # DisableMasterPasswordCreation = true;
+                    # DisableProfileImport = true;
+                    # DisableProfileRefresh = true;
+                    # DisableSetDesktopBackground = true;
+                    DisablePocket = true;
+                    DisableTelemetry = true;
+                    # DisableFormHistory = true;
+                    # DisablePasswordReveal = true;
+
+                    # UI and Behavior
+                    # DisplayMenuBar = "never";
+                    DontCheckDefaultBrowser = true;
+                    HardwareAcceleration = true;
+                    OfferToSaveLogins = false;
+
+                    # Extensions
+                    ExtensionSettings =
+                        let
+                            moz =
+                                short:
+                                "https://addons.mozilla.org/firefox/downloads/latest/${short}/latest.xpi";
+                        in
+                        {
+                            "*".installation_mode = "blocked";
+
+                            "uBlock0@raymondhill.net" = {
+                                install_url = moz "ublock-origin";
+                                installation_mode = "force_installed";
+                                updates_disabled = true;
+                            };
+
+                            "{f3b4b962-34b4-4935-9eee-45b0bce58279}" = {
+                                install_url = moz "animated-purple-moon-lake";
+                                installation_mode = "force_installed";
+                                updates_disabled = true;
+                            };
+
+                            "{73a6fe31-595d-460b-a920-fcc0f8843232}" = {
+                                install_url = moz "noscript";
+                                installation_mode = "force_installed";
+                                updates_disabled = true;
+                            };
+                        };
+
+                    # Extension configuration
+                    "3rdparty".Extensions = {
+                        "uBlock0@raymondhill.net".adminSettings = {
+                            userSettings = rec {
+                                uiTheme = "dark";
+                                uiAccentCustom = true;
+                                uiAccentCustom0 = "#8300ff";
+                                cloudStorageEnabled = lib.mkForce false;
+
+                                importedLists = [
+                                    "https:#filters.adtidy.org/extension/ublock/filters/3.txt"
+                                    "https:#github.com/DandelionSprout/adfilt/raw/master/LegitimateURLShortener.txt"
+                                ];
+
+                                externalLists = lib.concatStringsSep "\n" importedLists;
+                            };
+
+                            selectedFilterLists = [
+                                "CZE-0"
+                                "adguard-generic"
+                                "adguard-annoyance"
+                                "adguard-social"
+                                "adguard-spyware-url"
+                                "easylist"
+                                "easyprivacy"
+                                "https:#github.com/DandelionSprout/adfilt/raw/master/LegitimateURLShortener.txt"
+                                "plowe-0"
+                                "ublock-abuse"
+                                "ublock-badware"
+                                "ublock-filters"
+                                "ublock-privacy"
+                                "ublock-quick-fixes"
+                                "ublock-unbreak"
+                                "urlhaus-1"
+                            ];
                         };
                     };
                 };
