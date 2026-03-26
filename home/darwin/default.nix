ddf4fbf8af55b18bb6991a5049c3c58970a28987
Ian Pratt <ianjdpratt@gmail.com>
2026-03-26 00:48:30 +0000

    Nix rebuild

--- home/darwin/default.nix
+++ home/darwin/default.nix
@@ -4,9 +4,6 @@
 
     # MacOS specific-packages
     home.packages = with pkgs; [
-        raycast # Spotlight replacement (untill Spotlight recognizes nix packages)
-        net-news-wire # RSS reader
-        skimpdf # PDF viewer for nvim TODO: Make this multi-platform for nvim use on all devices.
         pinentry_mac # EZ pinentry for GPG
         mkalias
         openssl # TODO: Why?
--- system/darwin/launchd.nix
+++ system/darwin/launchd.nix
@@ -6,18 +6,6 @@
 { pkgs, ... }:
 {
     launchd.user.agents = {
-        # Open raycast on startup
-        raycast = {
-            command = "open ${pkgs.raycast}/Applications/Raycast.app";
-
-            serviceConfig = {
-                KeepAlive = false; # When it stops, do *NOT* restart it (otherwise it just keeps opening lmao :p)
-                RunAtLoad = true;
-                StandardOutPath = "/tmp/raycast.out.log";
-                StandardErrorPath = "/tmp/raycast.err.log";
-            };
-        };
-
         # Open tailscale on startup
         tailscale = {
             command = "open /Applications/Tailscale.app";
