f96c9f2eb35767108e167c918fc1235f12a38c13
Ian Pratt <ianjdpratt@gmail.com>
2026-03-27 03:19:12 +0000

    Nix rebuild

--- modules/hosts/macbook/configuration.nix
+++ modules/hosts/macbook/configuration.nix
@@ -23,6 +23,7 @@ in
                 pam.services.sudo_local = {
                     enable = true;
                     touchIdAuth = true;
+                    watchIdAuth = true;
                 };
             };
 
