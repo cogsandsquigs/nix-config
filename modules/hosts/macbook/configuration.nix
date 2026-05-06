b029f0e5efce96f694a56c2a3782f28c881580e1
Ian Pratt <ianjdpratt@gmail.com>
2026-05-06 13:17:42 +0100

    Nix rebuild

--- modules/hosts/macbook/configuration.nix
+++ modules/hosts/macbook/configuration.nix
@@ -54,7 +54,7 @@ in
                             # "${pkgs.firefox-unwrapped}/Applications/Firefox.app" # NOTE: See homebrew.nix for why it's `firefox-unwrapped`
                             "/Applications/Firefox.app" # NOTE: See homebrew.nix for why it's `firefox-unwrapped`
                             "${pkgs.obsidian}/Applications/Obsidian.app"
-                            "${pkgs.discord-canary}/Applications/Discord PTB.app" # "${pkgs.discord}/Applications/Discord.app"
+                            "${pkgs.discord-canary}/Applications/Discord Canary.app" # "${pkgs.discord}/Applications/Discord.app"
                             "/System/Applications/Messages.app"
                             "/Applications/WhatsApp.app" # "${pkgs.whatsapp-for-mac}/Applications/WhatsApp.app"
                             "/System/Applications/Calendar.app"
