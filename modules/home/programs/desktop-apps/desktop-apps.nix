392beb42cecc08636a8967a14351efe883cfc7cd
Ian Pratt <ianjdpratt@gmail.com>
2026-05-21 13:29:35 +0100

    Nix rebuild

--- modules/home/programs/desktop-apps/desktop-apps.nix
+++ modules/home/programs/desktop-apps/desktop-apps.nix
@@ -14,6 +14,7 @@
                 discord # currently on macos gets stuck on launch, keeps trying 2 upd (???) ptb and canary don't fix issue
                 obsidian
                 zoom-us
+                notion-app
 
                 # Fun
                 #spotify
