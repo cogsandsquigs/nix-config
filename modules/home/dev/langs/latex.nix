5899f698061b63716054989f2890b3392a624a77
Ian Pratt <ianjdpratt@gmail.com>
2026-07-03 23:46:52 +0100

    Nix rebuild

--- modules/home/dev/langs/latex.nix
+++ modules/home/dev/langs/latex.nix
@@ -1,5 +1,11 @@
 { pkgs, ... }: {
     home.packages = with pkgs; [
+
+        # LSPs and such
+        texlab # LSP
+        texpresso # Live rendering of .tex -> .pdf
+        texlivePackages.latexindent # Formatting
+
         (texlive.withPackages (
             ps: with ps; [
                 latex
