2b61beaef74aade0fe0426cc88337d41c4d82693
Ian Pratt <ianjdpratt@gmail.com>
2026-07-13 18:34:43 +0100

    Nix rebuild

--- modules/home/desktop-apps/browser.nix
+++ modules/home/desktop-apps/browser.nix
@@ -1,7 +1,9 @@
-{ ... }: {
+{ pkgs, ... }: {
     # NOTE: Add when ladybird browser becomes stable!!
     # home.packages = with pkgs; [ ladybird ];
 
     # On macOS, Firefox itself is installed via Homebrew — see
     # modules/system/darwin/desktop-apps.nix.
+
+    # home.packages = with pkgs; [ ungoogled-chromium ];
 }
--- modules/system/darwin/desktop-apps.nix
+++ modules/system/darwin/desktop-apps.nix
@@ -14,6 +14,7 @@
             casks = [
                 "whatsapp" # Updated more freq. than whatsapp-for-mac nix
                 "firefox"
+                "chromium"
                 #"google-drive" # Google drive GUI client
             ];
         };
