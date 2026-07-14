364b754146168335fd4995b6571295258667f54c
Ian Pratt <ian.pratt@arcticlake.com>
2026-07-14 09:05:49 +0100

    Nix rebuild

--- modules/home/fonts.nix
+++ modules/home/fonts.nix
@@ -10,11 +10,14 @@
     options.my.user.fonts.enable = tools.opt.mkEnabled "Fonts (Fira Code, Atkinson Hyperlegible)";
 
     config = lib.mkIf config.my.user.fonts.enable {
-        home = {
+        home-manager = {
+
             fonts = {
                 fontconfig.enable = true;
             };
+        };
 
+        home = {
             packages = with pkgs; [
                 nerd-fonts.fira-code
                 atkinson-hyperlegible # Old version
