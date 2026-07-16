04cc3ff297717070943549bb242dcb6a342b7ac3
Ian Pratt <ian.pratt@arcticlake.com>
2026-07-16 13:54:18 +0100

    Nix rebuild

--- modules/home/dev/ldap.nix
+++ modules/home/dev/ldap.nix
@@ -1,1 +1,24 @@
-{ pkgs, ... }: { home.packages = with pkgs; [ apache-directory-studio ]; }
+{
+    pkgs,
+    tools,
+    lib,
+    config,
+    ...
+}:
+{
+
+    options.my.user.dev.ldap.enable = tools.opt.mkRiding config.my.user.dev.enable "JetBrains IDEA";
+
+    config = lib.mkIf config.my.user.dev.ldap.enable {
+        home.packages = (
+            if
+                # Restriction necessary since apache-directory-studio only works on linux (for nix builds) :/
+                pkgs.stdenv.isLinux
+            then
+                with pkgs; [ apache-apache-directory-studio ]
+            else
+                [ ]
+        );
+    };
+
+}
