33d0ee4b03ae1cb68054b7f64e0c4de011d93602
Ian Pratt <ian.pratt@arcticlake.com>
2026-07-22 09:49:27 +0100

    Nix rebuild

--- modules/home/shell/prompt.nix
+++ modules/home/shell/prompt.nix
@@ -67,6 +67,13 @@
                 # Direnv detection
                 direnv = {
                     disabled = false;
+                    format = "[$symbol($loaded/$allowed)]($style)";
+                    symbol = "direnv ";
+                    allowed_msg = "✓";
+                    not_allowed_msg = "-";
+                    denied_msg = "✗";
+                    loaded_msg = "⬆";
+                    unloaded_msg = "⬇";
                 };
 
                 # Command duration
