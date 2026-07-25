9f270771c2258a21d89059b34bb9889031f98bd5
Ian Pratt <ianjdpratt@gmail.com>
2026-07-25 19:30:23 +0100

    Nix rebuild

--- modules/home/dev/langs/go.nix
+++ modules/home/dev/langs/go.nix
@@ -1,9 +1,14 @@
 { pkgs, ... }: {
-    lang = [ "go" ];
+    lang = [
+        "go"
+        "gomod"
+    ];
 
     pkgs = with pkgs; [
-        go
-        gopls
+        go # Bundles `gofmt`
+        gopls # Primary LSP
+        golangci-lint-langserver # Secondary LSP
+        delve # Debugger
     ];
 
     lsp = [
@@ -16,9 +21,31 @@
             ];
             config = {
                 "ui.diagnostic.staticcheck" = true;
+                hints = {
+                    assignVariableTypes = true;
+                    compositeLiteralFields = true;
+                    constantValues = true;
+                    functionTypeParameters = true;
+                    parameterNames = true;
+                    rangeVariableTypes = true;
+                };
+            };
+        }
+        {
+            name = "golangci-lint-lsp";
+            cmd = [ "golangci-lint-langserver" ];
+            config = {
+                command = [
+                    "golangci-lint"
+                    "run"
+                    "--output.json.path"
+                    "stdout"
+                    "--show-stats=false"
+                    "--issues-exit-code=1"
+                ];
             };
         }
     ];
 
-    fmt = [ ];
+    fmt = [ "gofmt" ];
 }
