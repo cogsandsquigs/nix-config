206de50951e430a6d6959056f570eaacbfb29f6a
Ian Pratt <ianjdpratt@gmail.com>
2026-07-27 20:07:06 +0100

    Nix rebuild

--- modules/system/darwin/games.nix
+++ modules/system/darwin/games.nix
@@ -16,6 +16,7 @@
                 "olympus" # Celeste mod loader # NOTE: for some reason not supported on nix aarch-64
                 "porting-kit" # Windows -> Mac games
                 "aaf2tbz/tap/metalsharp" # Windows -> Mac games (via Game Porting ToolKit)
+                "apple/apple/game-porting-toolkit" # Windows -> Mac games (Game Porting ToolKit)
             ];
         };
     };
